34e753d989b8d8e5ee84d1816fd97616a3f30653
sakshikukreja14 <sakshi.kukreja@elucidata.io>
2020-04-29 13:25:16 +0530

    [fix] Fixed a bug in undo function
    undo function misbehaved in case where peaks are marked good or bad
    by the user. This bug has been fixed.

--- src/gui/mzroll/tabledockwidget.cpp
+++ src/gui/mzroll/tabledockwidget.cpp
@@ -1726,10 +1726,20 @@ void TableDockWidget::showConsensusSpectra() {
 }
 
 void TableDockWidget::markGroupGood() {
-  setGroupLabel('g');
+
   auto currentGroups = getSelectedGroups();
+  
   if (currentGroups.isEmpty())
       return;
+
+  for(auto group : currentGroups)
+  {
+    auto label = group->labelToString(group->predictedLabel());
+    auto probability = group->predictionProbability();
+    pair<string, float> groupLabel = make_pair(label, probability);
+    undoBuffer[group->groupId] = groupLabel;
+  }
+  setGroupLabel('g');
 
   _mainwindow->getAnalytics()->hitEvent("Peak Group Curation", "Mark Good");
   showNextGroup();
@@ -1737,11 +1747,20 @@ void TableDockWidget::markGroupGood() {
 }
 
 void TableDockWidget::markGroupBad() {
-  setGroupLabel('b');
   auto currentGroups = getSelectedGroups();
+
   if (currentGroups.isEmpty())
       return;
 
+  for(auto group : currentGroups)
+  {
+    auto label = group->labelToString(group->predictedLabel());
+    auto probability = group->predictionProbability();
+    pair<string, float> groupLabel = make_pair(label, probability);
+    undoBuffer[group->groupId] = groupLabel;
+  }
+  setGroupLabel('b');
+  
   _mainwindow->getAnalytics()->hitEvent("Peak Group Curation", "Mark Bad");
   showNextGroup();
   _mainwindow->autoSaveSignal(currentGroups);
